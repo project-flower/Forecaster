c4a9eb1f2373bcfffca79a3498381351e576312a
Yuki Nishina <project_flower@outlook.com>
2025-11-23 23:43:12 +0900

    ListView を RecyclerView に移行

--- app/src/main/java/projectflower/forecaster/MainActivity.kt
+++ app/src/main/java/projectflower/forecaster/MainActivity.kt
@@ -9,12 +9,15 @@ import androidx.core.view.isVisible
 import androidx.datastore.preferences.core.edit
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import projectflower.forecaster.databinding.ActivityMainBinding
 import projectflower.forecaster.databinding.DateHeaderBinding
@@ -24,15 +27,18 @@ import java.util.Locale
 
 class MainActivity : AppCompatActivity(), AreaSelectDialogFragment.NoticeDialogListener {
 
+    // Private Fields
+
     private lateinit var addRowButton: FloatingActionButton
     private lateinit var binding: ActivityMainBinding
     private lateinit var dates: Array<LocalDate>
     private lateinit var editButton: FloatingActionButton
     private lateinit var endEditButton: FloatingActionButton
     private lateinit var headers: Array<DateHeaderBinding>
-    private lateinit var weatherListAdapter: WeatherListAdapter
-    private lateinit var updateButton: FloatingActionButton
     private lateinit var mainView: View
+    private lateinit var updateButton: FloatingActionButton
+    private var updateError: String? = null
+    private lateinit var weatherListAdapter: WeatherListAdapter
     private var weathersUpdateNeeded: Boolean = true
 
     // Over-ride Methods
@@ -59,8 +65,8 @@ class MainActivity : AppCompatActivity(), AreaSelectDialogFragment.NoticeDialogL
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         mainView = binding.root
+        setContentView(mainView)
 
         headers = arrayOf(
             binding.header1,
@@ -73,20 +79,28 @@ class MainActivity : AppCompatActivity(), AreaSelectDialogFragment.NoticeDialogL
             binding.header8
         )
 
+        val listView = binding.listview
+        listView.layoutManager = LinearLayoutManager(this)
+
+        weatherListAdapter = WeatherListAdapter(
+            this@MainActivity,
+            { position, area, childArea ->
+                onWeatherListViewEditClicked(
+                    position,
+                    area,
+                    childArea
+                )
+            },
+            { onWeathersUpdateCompleted() }
+        )
+
+        listView.adapter = weatherListAdapter
+        // listview.isClickable = true
+
         editButton = binding.fabEdit
         updateButton = binding.fabSync
         endEditButton = binding.fabEndEdit
         addRowButton = binding.fabAddRow
-
-        weatherListAdapter = WeatherListAdapter(
-            this@MainActivity,
-            // rows,
-            ArrayList(),
-            { position, area, childArea -> onWeatherListViewEditClicked(position, area, childArea) }
-        )
-
-        binding.listview.adapter = weatherListAdapter
-        binding.listview.isClickable = true
 
         editButton.setOnClickListener { setEditMode(true) }
         updateButton.setOnClickListener { updateWeathers() }
@@ -122,7 +136,7 @@ class MainActivity : AppCompatActivity(), AreaSelectDialogFragment.NoticeDialogL
 
     /** 行追加ボタンのタップ イベント リスナー */
     private fun onAddRowButtonClicked() {
-        weatherListAdapter.add(
+        weatherListAdapter.addItem(
             WeatherListDisplayData(
                 "",
                 "",
@@ -158,10 +172,10 @@ class MainActivity : AppCompatActivity(), AreaSelectDialogFragment.NoticeDialogL
         }
 
         // ここは DataStore.edit からも呼ばれるため、初めにクリアする
-        weatherListAdapter.clear()
+        weatherListAdapter.clearItems()
 
         decoded?.let {
-            weatherListAdapter.addAll(decoded.map { d ->
+            weatherListAdapter.addItems(decoded.map { d ->
                 WeatherListDisplayData(
                     d.displayName,
                     d.areaName,
@@ -175,8 +189,6 @@ class MainActivity : AppCompatActivity(), AreaSelectDialogFragment.NoticeDialogL
             })
         }
 
-        weatherListAdapter.notifyDataSetChanged()
-
         if (weathersUpdateNeeded) {
             updateWeathers()
         }
@@ -187,36 +199,12 @@ class MainActivity : AppCompatActivity(), AreaSelectDialogFragment.NoticeDialogL
     /** HTTP GET により気象予報情報を取得した時の処理をします。 */
     @UiThread
     private fun onWeatherDataReceived(areaCode: String, result: Map<String, Array<WeatherData>>) {
-        for (childArea in result) {
-            for (i in 0 until weatherListAdapter.count) {
-                val listData = weatherListAdapter.getItem(i)
-                val childCode = listData!!.childCode
+        weatherListAdapter.updateItems(areaCode, result, dates)
+    }
 
-                if (areaCode != listData.areaCode ||
-                    childCode != childArea.key
-                ) continue
-
-                val weathers = ArrayList<WeatherData>()
-
-                for (date in dates) {
-                    val weather = childArea.value.find { c -> c.date == date }
-
-                    if (weather == null) {
-                        weathers.add(
-                            WeatherData(areaCode, childCode, "", date, "", 0, "", R.drawable.blank)
-                        )
-                        continue
-                    }
-
-                    weathers.add(weather)
-                }
-
-                listData.weathers = weathers
-                listData.inProgress = false
-            }
-        }
-
-        weatherListAdapter.notifyDataSetChanged()
+    @UiThread
+    private fun onWeatherDataReceiveFailed(areaCode: String) {
+        weatherListAdapter.updateFailedItems(areaCode)
     }
 
     /** ListView 要素の編集ボタンのタップ イベント リスナー */
@@ -239,42 +227,32 @@ class MainActivity : AppCompatActivity(), AreaSelectDialogFragment.NoticeDialogL
      * @param error 更新時に発生したエラー
      */
     @UiThread
-    private fun onWeathersUpdateCompleted(error: String?) {
-        error?.let {
+    private fun onWeathersUpdateCompleted() {
+        updateError?.let {
             showErrorMessage(it)
         }
 
-        showProgressSpinner(false)
+        updateError = null
         enableButtons(true)
     }
 
     /** [savedData] を JSON 形式で Preference に書き込みます。 */
     private suspend fun saveJsonSettings(savedData: Array<WeatherListData>) {
-        this@MainActivity.dataStore.edit { settings ->
+        dataStore.edit { settings ->
             settings[Constants.settingsKey] = Json.encodeToString(savedData)
         }
     }
 
     /** 現在の ListView の設定を保存します。 */
     private fun saveSettings() {
-        val data = mutableListOf<WeatherListData>()
-
-        for (i in 0 until weatherListAdapter.count) {
-            weatherListAdapter.getItem(i)?.let { data.add(it) }
-        }
-
         lifecycleScope.launch {
-            saveJsonSettings(data.toTypedArray())
+            saveJsonSettings(weatherListAdapter.getSettings())
         }
     }
 
     /** 編集モードを有効化又は解除します。 */
     private fun setEditMode(enabled: Boolean) {
-        for (i in 0 until weatherListAdapter.count) {
-            weatherListAdapter.getItem(i)?.isEditing = enabled
-        }
-
-        weatherListAdapter.notifyDataSetChanged()
+        weatherListAdapter.isEditing = enabled
         editButton.isVisible = !enabled
         updateButton.isVisible = !enabled
         endEditButton.isVisible = enabled
@@ -287,16 +265,6 @@ class MainActivity : AppCompatActivity(), AreaSelectDialogFragment.NoticeDialogL
         Snackbar.make(mainView, error, Snackbar.LENGTH_LONG)
             .setAction("Action", null)
             .show()
-    }
-
-    /** ListView の各要素の表示内容を処理中にします。 */
-    private fun showProgressSpinner(show: Boolean) {
-        for (i in 0 until weatherListAdapter.count) {
-            val listData = weatherListAdapter.getItem(i)
-            listData?.inProgress = show
-        }
-
-        weatherListAdapter.notifyDataSetChanged()
     }
 
     /** 日付を現在日時を基準に更新します。 */
@@ -321,18 +289,7 @@ class MainActivity : AppCompatActivity(), AreaSelectDialogFragment.NoticeDialogL
         area: Pair<String, String>,
         childArea: Pair<String, String>
     ) {
-        val item = weatherListAdapter.getItem(itemPosition)!!
-        item.areaCode = area.first
-        item.areaName = area.second
-        item.childCode = childArea.first
-        item.childName = childArea.second
-
-        item.displayName = when (area.first == childArea.first) {
-            true -> area.second
-            else -> "${area.second} ${childArea.second}"
-        }
-
-        weatherListAdapter.notifyDataSetChanged()
+        weatherListAdapter.updateItem(itemPosition, area, childArea)
     }
 
     /** 気象予報を取得し、表示内容を更新します。 */
@@ -340,48 +297,34 @@ class MainActivity : AppCompatActivity(), AreaSelectDialogFragment.NoticeDialogL
     private fun updateWeathers() {
         enableButtons(false)
         synchronizeWeek()
-        showProgressSpinner(true)
-        val areaCodes = mutableMapOf<String, MutableSet<String>>()
-
-        for (i in 0 until weatherListAdapter.count) {
-            val listData = weatherListAdapter.getItem(i)
-            val areaCode = listData!!.areaCode
-
-            if (areaCode == "") continue // 追加しただけの行は空値になっているのでスキップ
-
-            var mapElement = areaCodes[areaCode]
-
-            if (mapElement == null) {
-                mapElement = mutableSetOf()
-            }
-
-            val childCode = listData.childCode
-
-            if (!mapElement.contains(childCode)) {
-                mapElement.add(childCode)
-            }
-
-            areaCodes[areaCode] = mapElement
-        }
+        updateError = null
+        weatherListAdapter.beginUpdating()
+        val areaCodes = weatherListAdapter.getAreaCodes()
 
         lifecycleScope.launch {
-            var error: String? = null
-
             for (areaCode in areaCodes) {
                 try {
                     val result = HttpConnector.getWeathers(areaCode.key, areaCode.value.toSet())
-                    onWeatherDataReceived(areaCode.key, result)
+                    withContext(Dispatchers.Main) {
+                        onWeatherDataReceived(
+                            areaCode.key,
+                            result
+                        )
+                    }
                 } catch (exception: Exception) {
+                    if (updateError.isNullOrEmpty()) {
+                        updateError = exception.message
+                    }
+
                     exception.message?.let {
                         Log.e("MainActivity", it)
                     }
 
-                    // ユーザーには最初のエラーだけを表示する
-                    error = error ?: exception.message
+                    withContext(Dispatchers.Main) {
+                        onWeatherDataReceiveFailed(areaCode.key)
+                    }
                 }
             }
-
-            onWeathersUpdateCompleted(error)
         }
     }
 }
--- app/src/main/java/projectflower/forecaster/WeatherListAdapter.kt
+++ app/src/main/java/projectflower/forecaster/WeatherListAdapter.kt
@@ -4,47 +4,92 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.annotation.UiThread
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.imageview.ShapeableImageView
+import java.time.LocalDate
 
 /**
  * @param onEditClicked リスト要素の編集ボタンがタップされた時のリスナー
  */
 class WeatherListAdapter(
     context: Context,
-    dataList: ArrayList<WeatherListDisplayData?>?,
-    private val onEditClicked: (position: Int, area: Pair<String, String>?, childArea: Pair<String, String>?) -> Unit
-) : ArrayAdapter<WeatherListDisplayData?>(context, R.layout.listview_item, dataList!!) {
-    // Over-ride Methods
+    private val onEditClicked: (position: Int, area: Pair<String, String>?, childArea: Pair<String, String>?) -> Unit,
+    private val onUpdateCompleted: () -> Unit
+) : RecyclerView.Adapter<WeatherListAdapter.ViewHolder>() {
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var view = convertView
-        val listData = getItem(position)!!
+    // Public Classes
 
-        if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.listview_item, parent, false)
-        }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Public Fields
 
-        val areaName = view.findViewById<TextView>(R.id.listName)
-        areaName.text = listData.displayName
+        /** 地域名 */
+        val areaName: TextView = itemView.findViewById(R.id.listName)
 
+        /** 削除ボタン */
+        val deleteButton: ShapeableImageView = itemView.findViewById(R.id.delete_area)
+
+        /** 編集ボタン */
+        val editButton: ShapeableImageView = itemView.findViewById(R.id.edit_area)
+
+        /** 気象アイコン */
         val images: Array<ImageView> = arrayOf(
-            view.findViewById(R.id.weather1),
-            view.findViewById(R.id.weather2),
-            view.findViewById(R.id.weather3),
-            view.findViewById(R.id.weather4),
-            view.findViewById(R.id.weather5),
-            view.findViewById(R.id.weather6),
-            view.findViewById(R.id.weather7),
-            view.findViewById(R.id.weather8)
+            itemView.findViewById(R.id.weather1),
+            itemView.findViewById(R.id.weather2),
+            itemView.findViewById(R.id.weather3),
+            itemView.findViewById(R.id.weather4),
+            itemView.findViewById(R.id.weather5),
+            itemView.findViewById(R.id.weather6),
+            itemView.findViewById(R.id.weather7),
+            itemView.findViewById(R.id.weather8)
         )
 
-        val inProgress = listData.inProgress
-        val isEditing = listData.isEditing
+        /** プログレス スピナー */
+        val progressBar: ProgressBar = itemView.findViewById(R.id.progressbar)
+    }
+
+    // Public Fields
+
+    val dataList: ArrayList<WeatherListDisplayData> = arrayListOf()
+
+    // Private Fields
+
+    private val inflater = LayoutInflater.from(context)
+    private var mInProgress = false
+
+    // Properties
+
+    /** 処理中にします。 */
+    val inProgress: Boolean
+        get() = mInProgress
+
+    /** 編集モードにします。 */
+    var isEditing: Boolean = false
+        get() = field
+        set(value) {
+            for (data in dataList) {
+                data.isEditing = value
+            }
+
+            field = value
+            notifyItemRangeChanged(0, dataList.size)
+        }
+
+    // Over-ride Methods
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val current = dataList[position]
+        val areaName = holder.areaName
+        areaName.text = current.displayName
+        val images = holder.images
 
         for (image in images) {
             if (image.isVisible == (inProgress || isEditing)) {
@@ -56,54 +101,38 @@ class WeatherListAdapter(
             }
         }
 
-        val editButton: ShapeableImageView = view.findViewById(R.id.edit_area)
+        val editButton: ShapeableImageView = holder.editButton
         editButton.isVisible = isEditing
-        // Click リスナを設定した後から並べ替えられると position は変わるのでボタンに位置を記憶させる
-        editButton.tag = position
 
-        if (!editButton.hasOnClickListeners()) {
-            editButton.setOnClickListener { view ->
-                val currentPosition = view.tag as Int
-                val item = getItem(currentPosition)!!
+        editButton.setOnClickListener { view ->
+            val data = dataList[position]
 
-                val area = when (item.areaCode != "") {
-                    true -> Pair(item.areaCode, item.areaName)
-                    else -> null
-                }
-
-                val childArea = when (area != null && item.childCode != "") {
-                    true -> Pair(item.childCode, item.childName)
-                    else -> null
-                }
-
-                onEditClicked(
-                    currentPosition,
-                    area,
-                    childArea
-                )
+            val area = when (data.areaCode != "") {
+                true -> Pair(data.areaCode, data.areaName)
+                else -> null
             }
+
+            val childArea = when (area != null && data.childCode != "") {
+                true -> Pair(data.childCode, data.childName)
+                else -> null
+            }
+
+            onEditClicked(
+                position, area, childArea
+            )
         }
 
-        val deleteButton: ShapeableImageView = view.findViewById(R.id.delete_area)
+        val deleteButton: ShapeableImageView = holder.deleteButton
         deleteButton.isVisible = isEditing
-        deleteButton.tag = position
 
-        if (!deleteButton.hasOnClickListeners()) {
-            deleteButton.setOnClickListener { view ->
-                val currentPosition = view.tag as Int
-                remove(getItem(currentPosition))
-                notifyDataSetChanged()
-            }
+        deleteButton.setOnClickListener { view ->
+            dataList.removeAt(position)
+            notifyItemRemoved(position)
         }
 
-        val progressBar: ProgressBar = view.findViewById(R.id.progressbar)
-        progressBar.isVisible = inProgress
-
-        if (inProgress) {
-            return view
-        }
-
-        val weathers = listData.weathers
+        val progressBar: ProgressBar = holder.progressBar
+        progressBar.isVisible = current.inProgress
+        val weathers = current.weathers
 
         for (i in 0 until images.size) {
             var imageId = R.drawable.blank
@@ -115,8 +144,173 @@ class WeatherListAdapter(
             }
 
             images[i].setImageResource(imageId)
+            images[i].isVisible = !(current.inProgress || isEditing)
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup, viewType: Int
+    ): ViewHolder {
+        return ViewHolder(inflater.inflate(R.layout.listview_item, parent, false))
+    }
+
+    // Public Methods
+
+    fun addItem(item: WeatherListDisplayData) {
+        dataList.add(item)
+        notifyItemInserted(dataList.size - 1)
+    }
+
+    fun addItems(items: List<WeatherListDisplayData>) {
+        if (items.isEmpty()) return
+
+        val position = dataList.size
+        dataList.addAll(items)
+        notifyItemRangeInserted(position, items.size)
+    }
+
+    fun beginUpdating() {
+        for (data in dataList) {
+            if (!data.areaCode.isEmpty()) {
+                data.inProgress = true
+            }
         }
 
-        return view
+        mInProgress = true
+        notifyItemRangeChanged(0, dataList.size)
+    }
+
+    fun cancelUpdating() {
+        for (data in dataList) {
+            data.inProgress = false
+        }
+
+        mInProgress = false
+        notifyItemRangeChanged(0, dataList.size)
+    }
+
+    fun clearItems() {
+        val itemCount = dataList.size
+        dataList.clear()
+        notifyItemRangeRemoved(0, itemCount)
+    }
+
+    fun getAreaCodes(): Map<String, MutableSet<String>> {
+        val result = mutableMapOf<String, MutableSet<String>>()
+
+        for (listData in dataList) {
+            val areaCode = listData.areaCode
+
+            if (areaCode == "") continue // 追加しただけの行は空値になっているのでスキップ
+
+            var mapElement = result[areaCode]
+
+            if (mapElement == null) {
+                mapElement = mutableSetOf()
+            }
+
+            val childCode = listData.childCode
+
+            if (!mapElement.contains(childCode)) {
+                mapElement.add(childCode)
+            }
+
+            result[areaCode] = mapElement
+        }
+
+        return result
+    }
+
+    fun getSettings(): Array<WeatherListData> {
+        if (dataList.isEmpty()) return arrayOf()
+
+        val result = mutableListOf<WeatherListData>()
+
+        for (data in dataList) {
+            result.add(data)
+        }
+
+        return result.toTypedArray()
+    }
+
+    fun updateFailedItems(areaCode: String) {
+        if (dataList.isEmpty()) return
+
+        for (i in 0 until dataList.size) {
+            val data = dataList[i]
+
+            if (data.areaCode == areaCode) {
+                data.inProgress = false
+                notifyItemChanged(i)
+                dataList[i] = data
+            }
+        }
+
+        notifyUpdateCompleted()
+    }
+
+    fun updateItem(itemPosition: Int, area: Pair<String, String>, childArea: Pair<String, String>) {
+        val item = dataList[itemPosition]
+        item.areaCode = area.first
+        item.areaName = area.second
+        item.childCode = childArea.first
+        item.childName = childArea.second
+
+        item.displayName = when (area.first == childArea.first) {
+            true -> area.second
+            else -> "${area.second} ${childArea.second}"
+        }
+
+        dataList[itemPosition] = item
+        notifyItemChanged(itemPosition)
+    }
+
+    fun updateItems(
+        areaCode: String,
+        result: Map<String, Array<WeatherData>>,
+        dates: Array<LocalDate>
+    ) {
+        if (dataList.isEmpty()) return
+
+        for (childArea in result) {
+            for (position in 0 until dataList.size) {
+                val data = dataList[position]
+                val childCode = data.childCode
+
+                if (areaCode != data.areaCode || childCode != childArea.key) continue
+
+                val weathers = ArrayList<WeatherData>()
+
+                for (date in dates) {
+                    val weather = childArea.value.find { c -> c.date == date }
+
+                    if (weather == null) {
+                        weathers.add(
+                            WeatherData(areaCode, childCode, "", date, "", 0, "", R.drawable.blank)
+                        )
+                        continue
+                    }
+
+                    weathers.add(weather)
+                }
+
+                data.weathers = weathers
+                data.inProgress = false
+                dataList[position] = data
+                notifyItemChanged(position)
+            }
+        }
+
+        notifyUpdateCompleted()
+    }
+
+    // Private Methods
+
+    @UiThread
+    private fun notifyUpdateCompleted() {
+        if (dataList.any { it.inProgress }) return
+
+        mInProgress = false
+        onUpdateCompleted()
     }
 }
