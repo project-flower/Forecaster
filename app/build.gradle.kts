d9fb968316859c78be09362f93bfb18943df3b08
Yuki Nishina <project_flower@outlook.com>
2025-11-25 23:23:42 +0900

    バージョン情報を更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "projectflower.forecaster"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
